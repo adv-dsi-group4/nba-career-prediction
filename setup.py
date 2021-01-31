d905fd22bd09fb59b291e87ee7fc87b058d5b412
HninPwint <hninptgmail.com>
2021-01-31 13:29:37 +1100

    Git init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building ML model to predict if a rookie player will last at least 5 years in the league based on his stats.',
+    author='Adv DSI Group 4',
+    license='MIT',
+)
